ab32f0f239c4b4541b6e83e8e388c8d655dcd036
nishkarta <nishkarta@gmail.com>
2026-02-18 09:06:14 +0700

    feat: handling logout

--- src/shared/layout/Header.tsx
+++ src/shared/layout/Header.tsx
@@ -4,9 +4,11 @@ import useWindowSize from "@shared/hooks/useWindowSize"
 import Icon from "@shared/ui/Icon"
 import { cx } from "@shared/utils/cx"
 import { useEffect, useRef, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function Header() {
   const { width } = useWindowSize()
+  const navigate = useNavigate()
 
   const [showProfileDropdown, setShowProfileDropdown] = useState(false)
 
@@ -22,7 +24,7 @@ export default function Header() {
   }, [clickedOutside, showProfileDropdown])
 
   return (
-    <header className="header bg-header sticky top-0 z-20 text-light-primary px-5 py-1.5 md:px-10 md:py-3 lg:px-20 lg:py-6">
+    <header className="header bg-header sticky top-0 z-25 text-light-primary px-5 py-1.5 md:px-10 md:py-3 lg:px-20 lg:py-6">
       <nav className="flex gap-2 items-center">
         <ul className="nav-list grow flex items-center gap-3 py-3 md:py-0 md:gap-10 lg:gap-20">
           <li className="nav-item logo">
@@ -63,7 +65,7 @@ export default function Header() {
           {
             showProfileDropdown
             &&
-            <div onClick={(e)=> e.stopPropagation()} className="profile-dropdown-menu absolute flex flex-col top-full right-0 bg-header py-1 rounded-sm">
+            <div onClick={(e) => e.stopPropagation()} className="profile-dropdown-menu absolute flex flex-col top-full right-0 bg-header py-1 rounded-sm">
               {
                 [
                   {
@@ -79,11 +81,14 @@ export default function Header() {
                   {
                     label: "Keluar",
                     icon: "logout",
-                    onClick: () => { }
+                    onClick: () => {
+                      localStorage?.clear()
+                      navigate('/login')
+                    }
                   },
                 ]
                   ?.map((each, i) => (
-                    <li key={i} className="profile-dropdown-item flex items-center gap-1 whitespace-nowrap px-3 py-2 cursor-pointer hover:text-primary">
+                    <li key={i} onClick={each?.onClick} className="profile-dropdown-item flex items-center gap-1 whitespace-nowrap px-3 py-2 cursor-pointer hover:text-primary">
                       <Icon icon={each?.icon} className="h-4 md:h-5 lg:h-6" />
                       <span className="text-[10px] md:text-[12px] lg:text-[14px]">{each?.label}</span>
                     </li>
